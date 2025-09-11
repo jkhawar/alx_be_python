76e28b70f67e72eb4f2e118ce22dd7dd2c939e9c
Khawar Jamil <j_khawar@hotmail.com>
2025-09-10 21:49:20 -0400

    Basic Arithmetic Exercise

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,5 +3,6 @@ number2 = 5
 
 print("Addition of", number1 ,"and" , number2 ,"is" , (number1+number2))
 print("Subtraction of" , number1 , "and" , number2 , "is" , (number1-number2))
-print("Multiplication of" , number1 , "and" , number2 , "is" , (number1*number2))
+print(f"Multiplication of {number1} and {number2} is {number1*number2}")
+
 
